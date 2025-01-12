65e1134d1cfa73bd38d6ab7ecaf6e245835e8d56
ryunosuke <shark747474@gmail.com>
2025-01-13 01:44:42 +0900

    add addGraph func

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
 import { useEffect, useState } from 'react'
-import { Prefecture, prefectures } from './api/prefectureApi'
+import { Prefecture, fetchPrefectures } from './api/prefectureApi'
 import './App.css'
+import { fetchPopulations } from './api/populationApi'
+
+interface GraphValues {
+  totalPopulation: GraphValue[];
+  youngPopulation: GraphValue[];
+  workingAgePopulation: GraphValue[];
+  elderlyPopulation: GraphValue[];
+}
+
+interface GraphValue {
+  prefCode: number;
+  x: string;
+  y: string;
+}
+
+function addGraph(prefCode: number) {
+  fetchPopulations(prefCode)
+    .then(data => {
+      console.log("addGraph");
+      console.log(data);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+}
 
 function App() {
   const [checkboxPrefectures, setCheckboxPrefectures] = useState<Prefecture[]>([]);
+  const [graphValues, setGraphValues] = useState<GraphValues>();
 
   useEffect(() => {
-    prefectures()
+    fetchPrefectures()
       .then(data => {
         setCheckboxPrefectures(data.result);
       })
@@ -17,10 +43,21 @@ function App() {
 
   return (
     <div>
-      <h1>Prefectures</h1>
+      <h3>Prefectures</h3>
       <div className='prefecture-checkboxes'>
         {checkboxPrefectures.map((pref) => (
-          <p>{pref.prefName}</p>
+          <label>
+            <input
+              type="checkbox"
+              value={pref.prefCode}
+              onChange={({target}) => {
+                if (target.checked) {
+                  addGraph(pref.prefCode);
+                }
+              }}
+            />
+            {pref.prefName}
+          </label>
         ))}
       </div>
     </div>
--- src/api/prefectureApi.ts
+++ src/api/prefectureApi.ts
@@ -10,7 +10,7 @@ export interface Prefecture {
   prefName: string;
 }
 
-export const prefectures = async (): Promise<PrefectureResponse> => {
+export const fetchPrefectures = async (): Promise<PrefectureResponse> => {
     const response = await fetch(
         "https://yumemi-frontend-engineer-codecheck-api.vercel.app/api/v1/prefectures",
         {
